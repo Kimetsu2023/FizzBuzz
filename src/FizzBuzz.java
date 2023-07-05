ca003de656f7a1a05cd69331c30cf402f45487f8
Masayuki Emoto <137867861+kimetsu2023@users.noreply.github.com>
2023-07-05 18:23:29 +0900

    課題提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,7 +1,22 @@
 
 public class FizzBuzz {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+        for (int number = 1; number <= 100; number++) {
+        	 
+	if (number % 15 == 0){
+		System.out.println("FizzBuzz");
 	}
-
+	else if (number % 5 == 0) {
+		System.out.println("Buzz");
+		}
+	else if(number % 3 == 0) {
+	
+		System.out.println("Fizz");
+	}
+	else {
+		System.out.println(number);
+	}
+        }
+	}
 }
+	
